9e942d4d395bd5038f3d5ac095bbaf80082d8165
leroyhermans <l.g.j.hermans@gmail.com>
2016-10-01 21:48:58 +0200

    buildmapping

--- truffle.js
+++ truffle.js
@@ -1,13 +1,20 @@
 module.exports = {
   build: {
     "index.html": "index.html",
+    "login.html": "login.html",
+    "rdw.html": "rdw.html",
+    "facturer_overview.html": "facturer-overview.html",
+    "facturer_review.html": "facturer-review.html",
+    "review.html": "review.html",
+    "details.html": "details.html",
+    "consumer.html": "consumer.html",
     "app.js": [
       "javascripts/app.js"
     ],
-    "style.css": [
+    "css/style.css": [
       "stylesheets/style.css"
     ],
-    "images/": "images/"
+    "img/": "images/"
   },
   rpc: {
     host: "localhost",
